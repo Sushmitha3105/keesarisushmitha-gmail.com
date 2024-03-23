8ce7bbf443d780e8ed9a6f684a535dc5d35f86da
Sushmitha <keesarisushmitha@gmail.com>
2024-03-23 14:27:56 +0530

    Add files via upload

---
+++ StepsCount.java
@@ -0,0 +1,15 @@
+package com.meghana;
+
+class StepsCount{
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(climbStairs(n));
+
+    }
+    public static int climbStairs(int n) {
+            if(n==0 || n==1){
+                return 1;
+            }
+            return climbStairs(n-1)+climbStairs(n-2);
+        }
+}
