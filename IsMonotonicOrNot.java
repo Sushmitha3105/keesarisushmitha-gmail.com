eb3f6c8dce3be23cfea8d7990697382480b11533
Sushmitha <keesarisushmitha@gmail.com>
2024-03-25 14:28:15 +0530

    Add files via upload

---
+++ IsMonotonicOrNot.java
@@ -0,0 +1,26 @@
+package com.meghana;
+
+class IsMonotonicOrNot{
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(isMonotonic(nums));
+    }
+    public  static boolean isMonotonic(int[] nums) {
+        if(nums[0] < nums[nums.length-1]){
+            for(int i =1; i <nums.length;i++){
+                if(nums[i]<nums[i-1]){
+                    return false;
+                }
+            }
+        }
+        else{
+            for(int i =1;i<nums.length;i++){
+                if(nums[i]>nums[i-1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+}
